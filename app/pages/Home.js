6c382975abccf4c0083bd0b18e218a79554aa4d8
navrajsingh4 <119980499+navrajsingh4@users.noreply.github.com>
2023-11-27 12:22:35 -0700

    phase1

---
+++ app/pages/Home.js
@@ -0,0 +1,23 @@
+import {React} from "react";
+import './HomeStyles.css'; // Importing the CSS file\
+import shoImage from 'app/pages/sho.png'; // Correct path to your image
+
+
+function Home() {
+  const handleOrderNow = () => {
+    console.log("Order Now button clicked");
+  };
+
+  return (
+    <div className="home">
+    <img src={shoImage} alt="Background" />
+      <div className="headerContainer">
+        <h1> LIV Shoes </h1>
+        <p> SHOES TO FIT ANY TASTE</p>
+        <button onClick={handleOrderNow()}> ORDER NOW</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
